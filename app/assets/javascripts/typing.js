548da63a79fdfc4a7e4a8bf25ab976b04c877308
kninomiya <kounin2017@gmail.com>
2018-05-08 17:30:17 +0900

    Internal configuration-1

--- app/assets/javascripts/typing.js
+++ app/assets/javascripts/typing.js
@@ -13,12 +13,13 @@ var cnt = 0; // 文字のカウント
 var cntmax = 0; // 文字列の文字数をカウントする
 var mondai = '';
 var timer1;// タイマーを格納する変数
-document.onkeydown = typeGame; // キーを押したら関数typeGame()を呼び出す
+document.onkeypress = typeGame; // キーを押したら関数typeGame()を呼び出す
 
 // カウントダウン関数を1000ミリ毎に呼び出す関数
 /* eslint no-unused-vars: "off" */
 function cntStart() {
-  document.timer.elements[1].disabled = true;
+  //document.timer.elements[1].disabled = true;
+  $("#start").attr("disabled",  true);
   //  timer1 = setInterval('countDown()', 1000);
   timer1 = setInterval(() => {
     countDown();
@@ -28,15 +29,18 @@ function cntStart() {
 // タイマー停止関数
 /* eslint no-unused-vars: "off" */
 function cntStop() {
-  document.timer.elements[1].disabled = false;
+  //document.timer.elements[1].disabled = false;
+  $("#start").attr("disabled",  false);
   clearInterval(timer1);
 }
 
 // カウントダウン関数
 /* eslint no-unused-vars: "off" */
 function countDown() {
-  var sec = document.timer.elements[0].value;
-
+  //var sec = document.timer.elements[0].value;
+  var sec = $("#leftSeconds").val();
+  console.log(11);
+  console.log(sec);
   if (sec === '') {
     alert('時刻を設定してください！');
     reSet();
@@ -53,22 +57,31 @@ function countDown() {
 function tmWrite(int) {
   var mini;
   mini = parseInt(int, 10);
+  console.log(mini);
 
   if (int <= 0) {
     reSet();
     alert('時間です！');
   } else {
     // 残り秒数はintを60で割った余り
-    document.timer.elements[0].value = mini % 60;
-    document.getElementById('timercount').innerHTML = document.timer.elements[0].value;
+    $("#leftSeconds").attr("value",  mini % 60);
+    console.log(mini % 60);
+    $("#timerCount").html(mini % 60);
+
+    //document.timer.elements[0].value = mini % 60;
+    //document.getElementById('timercount').innerHTML = document.timer.elements[0].value;
+    //$("#timercount").html(("#leftSeconds").attr("value",  mini % 60));
   }
 }
 
 // フォームを初期状態に戻す(リセット)関数
 /* eslint no-unused-vars: "off" */
 function reSet() {
-  document.timer.elements[0].value = '0';
-  document.timer.elements[1].disabled = false;
+  //document.timer.elements[0].value = '0';
+  var leftseconds = $("#leftseconds").attr("value",  0);
+  console.log(leftseconds);
+  //document.timer.elements[1].disabled = false;
+  $("#start").attr("disabled",  false);
   clearInterval(timer1);
 }
 
@@ -89,7 +102,7 @@ function gameSet() {
   console.log(mondai);
 
   // 問題文を枠の中に入れる
-  document.getElementById('waku').innerHTML = mondai;
+  $("#waku").html(mondai);
   console.log(mondai);
 }
 // キー入力を受け取る関数
@@ -100,12 +113,14 @@ function typeGame(evt) {
   var idName = 'waku';
   console.log(102);
   console.log(mondai);
-  var elements = document.getElementsByTagName('*');
+  //var elements = document.getElementsByTagName('*');
+  var elements = $('*');
   console.log(elements);
   console.log(mondai);
   // 入力されたキーのキーコードを取得
   if (elements) {
     kc = evt.keyCode;
+    console.log(evt.keyCode);
     console.log(103);
     console.log(kc);
     console.log(mondai);
@@ -114,29 +129,36 @@ function typeGame(evt) {
     console.log(104);
   }
   // 入力されたキーコードと問題文のキーコードを比較
+  console.log(1001);
+  console.log(cnt);
   console.log(kc);
-  console.log(mondai.charCodeAt(cnt));
-  if (kc === mondai.charCodeAt(cnt)) {
+  console.log(mondai.charCodeAt(0));
+  if (kc === mondai.charCodeAt(0)) {
     console.log(105);
     // 入力されたセルの文字色を灰色にする
-    idName = mondai.substr(cnt, cnt + 1);
-    document.getElementById(idName).style.color = '#808080';
+    idName = mondai.slice(0, 1);
+    console.log(cnt);
+    console.log(idName);
+    console.log(mondai.slice(0, 1));
+    $("#" + idName).css("color", "#808080");
     cnt += 1; // 文字列を1つ進める
+    mondai = mondai.substring(1, mondai.Length);
   } else {
-    // 入力されたセルの文字色を赤にしdName = mondai.substr(cnt,cnt+1);var idName = mondai[cnt];
+    // 入力されたセルの文字色を赤にする（まだ出来てない）
     console.log(106);
     console.log(mondai);
-    console.log(mondai.substr(cnt, cnt + 1));
+    console.log(mondai.slice(0, 1));
     console.log(idName);
     console.log(cnt);
-    console.log(document.getElementById(idName));
-    document.getElementById(idName).style.color = '#FF0000';
+    console.log($("#idName"));
+    $("#" + idName).css("color", "#FF0000");
   }
 
   // 全文字入力したか確認、入力していたら次の写真と文字列を呼び出す
+    console.log(cntmax);
   if (cnt < cntmax) {
     console.log(107);
-    mondai = mondai.substring(1, mondai.Length);
+    console.log(mondai);
   } else {
     console.log(108);
     const a = Math.random() * 3;
@@ -144,12 +166,19 @@ function typeGame(evt) {
     $('#title_img').attr('src', window.questions[n].image_path);
     const newText = window.questions[n].character_string;
     $('#string').text(newText);
+    $('#waku').text(newText);
+    console.log(newText);
+    mondai = newText;
+    console.log(mondai);
+    cnt = 0;
+    cntmax = mondai.length;
   }
 
   // 残り時間が0になったらゲーム画面にGAME OVERの文字を出力する
-  if (document.timer.elements[0].value === 0) {
+  //if (document.timer.elements[0].value === 0) {
+    if ($("#leftSeconds").attr("value") === 0) {
     console.log(109);
     const fin = 'TIME OVER';
-    document.getElementById('waku').innerHTML = fin;
+    $("#waku").html(fin);
   }
 }
