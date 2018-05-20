3bbd4ff26c81845f299777aa1cbe955ec6809070
kninomiya <kounin2017@gmail.com>
2018-05-21 00:08:55 +0900

    layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+      :registrations => "registrations"
+  }
   resources :users, only: [:show]
   get 'users/show'
   get 'users/new'
